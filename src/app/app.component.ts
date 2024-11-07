93664fc4259a6a945d29a30dfae26627776efd14
Thomas Dijkmans <tdijkmans@gmail.com>
2024-11-07 17:28:34 +0100

    feat: replace ngx-charts with ng-apexcharts and update chart implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,41 @@
 import { CommonModule } from "@angular/common";
-import { Component, type OnInit } from "@angular/core";
+import { Component, type OnInit, ViewChild } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
 import {
-  type Color,
-  NgxChartsModule
-} from "@swimlane/ngx-charts";
+  type ApexOptions,
+  type ChartComponent,
+  NgApexchartsModule,
+} from "ng-apexcharts";
 import type { LapDto } from "./models";
 import { DataService, type SkaterData } from "./services/data.service";
 
 const styles = getComputedStyle(document.documentElement);
-const colorScheme = {
-  domain: [
-    styles.getPropertyValue("--data-color-primary"),
-    styles.getPropertyValue("--data-color-secondary"),
-    styles.getPropertyValue("--data-color-tertiary"),
-    styles.getPropertyValue("--data-color-quaternary"),
-    styles.getPropertyValue("--data-color-quinary"),
-    styles.getPropertyValue("--data-color-senary"),
-  ],
-} as Color;
-
-const colorSchemeTwo = { domain: [colorScheme.domain[1]], } as Color;
+const primaryColor = styles.getPropertyValue("--data-color-primary");
+const secondaryColor = styles.getPropertyValue("--data-color-secondary");
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, NgxChartsModule, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, NgApexchartsModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
   providers: [DataService],
 })
 export class AppComponent implements OnInit {
   title = "";
-  view = [800, 400] as [number, number];
-  colorScheme = {} as Color;
-  colorSchemeTwo = {} as Color;
   textValue = "";
   errorMessage = "";
-  lapColumns: { name: number; value: number }[] = [];
 
-  constructor(private dataService: DataService) { }
+  @ViewChild("chart") chart: ChartComponent = {} as ChartComponent;
+
+  chartOptions: Partial<ApexOptions> = {} as ApexOptions;
+
+  constructor(private dataService: DataService) {
+
+  }
 
   ngOnInit() {
-    this.colorScheme = colorScheme;
-    this.colorSchemeTwo = colorSchemeTwo;
     const chipCode = localStorage.getItem("chipCode");
     if (chipCode) {
       this.fetchLaps(chipCode);
@@ -79,13 +70,98 @@ export class AppComponent implements OnInit {
     }).format(date);
 
     const laps = sessions.flatMap((session) => session.laps || []);
-    console.log(laps);
-
-
-    this.lapColumns = laps.map((lap: LapDto, i) => ({
-      name: i + 1,
-      value: this.dataService.durationToSeconds(lap.duration || "0:00"),
-    }))
+    this.setChartOptions(laps);
   }
 
+  setChartOptions(laps: LapDto[]) {
+    const twoDecimal = (v: number) => Math.round(v * 100) / 100;
+
+    const lapDurations = laps
+      .map((lap: LapDto) =>
+        this.dataService.durationToSeconds(lap.duration || "0:00"),
+      )
+      .map((v) => twoDecimal(v));
+
+    const lapSpeeds = laps
+      .map((lap) => lap.speed?.kph || 0)
+      .map((v) => twoDecimal(v));
+
+    const categories = lapDurations
+      .map((v, i) => i + 1)
+      .map((v, i) =>
+        i === 0 || i % 5 === 0 || i === lapDurations.length - 1 ? v : "",
+      );
+
+    this.chartOptions = {
+      plotOptions: {
+        bar: {
+          borderRadius: 3,
+          borderRadiusApplication: "end",
+        },
+      },
+      series: [
+        {
+          name: "Rondetijd",
+          type: "column",
+          data: lapDurations,
+          color: primaryColor,
+        },
+        {
+          name: "Sneldheid",
+          type: "column",
+          data: lapSpeeds,
+          color: secondaryColor,
+        },
+      ],
+      chart: { type: "bar", toolbar: { show: false } },
+      dataLabels: { enabled: false },
+      xaxis: { categories },
+
+      yaxis: [
+        {
+          axisTicks: { show: true },
+          axisBorder: { show: true, color: primaryColor },
+          labels: {
+            style: {
+              colors: primaryColor,
+            },
+          },
+          decimalsInFloat: 0,
+          title: {
+            text: "Rondetijd (sec)",
+            style: {
+              fontWeight: 400,
+              fontSize: "1rem",
+              fontFamily: "Roboto, sans-serif",
+            },
+          },
+        },
+        {
+          seriesName: "Snelheid",
+          opposite: true,
+          axisTicks: { show: true },
+          axisBorder: { show: true, color: secondaryColor },
+          labels: {
+            style: {
+              colors: secondaryColor,
+            },
+          },
+          decimalsInFloat: 0,
+          title: {
+            text: "Sneldheid (km/h)",
+            style: {
+              fontWeight: 400,
+              fontSize: "1rem",
+              fontFamily: "Roboto, sans-serif",
+            },
+          },
+        },
+      ],
+
+      legend: {
+        horizontalAlign: "left",
+        offsetX: 40,
+      },
+    };
+  }
 }
