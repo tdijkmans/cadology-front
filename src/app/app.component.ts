fd0c9398470396d7d9ff4ddcfbab6798134f4acd
Thomas Dijkmans <tdijkmans@gmail.com>
2024-11-07 14:38:52 +0100

    feat: add durationToSeconds method to convert duration strings to seconds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,9 @@ import { FormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
 import {
   type Color,
-  LegendPosition,
-  LineChartComponent,
-  NgxChartsModule,
+  NgxChartsModule
 } from "@swimlane/ngx-charts";
-import { LapDto } from "./models";
-import { data } from "./services/data.interface";
+import type { LapDto } from "./models";
 import { DataService, type SkaterData } from "./services/data.service";
 
 const styles = getComputedStyle(document.documentElement);
@@ -24,6 +21,8 @@ const colorScheme = {
   ],
 } as Color;
 
+const colorSchemeTwo = { domain: [colorScheme.domain[1]], } as Color;
+
 @Component({
   selector: "app-root",
   standalone: true,
@@ -34,19 +33,18 @@ const colorScheme = {
 })
 export class AppComponent implements OnInit {
   title = "";
-  results = data;
   view = [800, 400] as [number, number];
-  legend = true;
-  legendPosition = LegendPosition.Below;
   colorScheme = {} as Color;
+  colorSchemeTwo = {} as Color;
   textValue = "";
   errorMessage = "";
-  laps: LineChartComponent["results"] = [];
+  lapColumns: { name: number; value: number }[] = [];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.colorScheme = colorScheme;
+    this.colorSchemeTwo = colorSchemeTwo;
     const chipCode = localStorage.getItem("chipCode");
     if (chipCode) {
       this.fetchLaps(chipCode);
@@ -70,9 +68,9 @@ export class AppComponent implements OnInit {
   }
 
   processData(skaterData?: SkaterData) {
-    const firstSession = skaterData?.latestActivity?.sessions?.[0] || {};
+    const sessions = skaterData?.latestActivity?.sessions || [];
+    const date = new Date(sessions[0].dateTimeStart || "");
 
-    const date = new Date(firstSession.dateTimeStart || "");
     this.title = Intl.DateTimeFormat("nl-NL", {
       weekday: "long",
       year: "numeric",
@@ -80,50 +78,14 @@ export class AppComponent implements OnInit {
       day: "numeric",
     }).format(date);
 
-    const laps = firstSession.laps || [];
+    const laps = sessions.flatMap((session) => session.laps || []);
+    console.log(laps);
 
-    this.laps = [
-      {
-        name: "Snelheid",
-        series: laps.map((lap: LapDto) => ({
-          name: lap.nr,
-          value: lap.speed?.kph,
-        })),
-      },
-      {
-        name: "Rondetijd",
-        series: laps.map((lap: LapDto) => ({
-          name: lap.nr,
-          value: this.durationToSeconds(lap.duration || "0:00"),
-        })),
-      },
-    ];
+
+    this.lapColumns = laps.map((lap: LapDto, i) => ({
+      name: i + 1,
+      value: this.dataService.durationToSeconds(lap.duration || "0:00"),
+    }))
   }
 
-  durationToSeconds(duration: string): number {
-    // Regular expression to match "minutes:seconds.milliseconds" or "seconds.milliseconds"
-    const regex = /^(\d+):(\d{1,2})\.(\d{1,3})$|^(\d+)\.(\d{1,3})$/;
-    const match = duration.match(regex);
-
-    if (!match) {
-      throw new Error("Invalid duration format");
-    }
-
-    if (match[1]) {
-      // Case with minutes and seconds (e.g., "1:10.360")
-      const minutes = Number.parseInt(match[1], 10);
-      const seconds = Number.parseInt(match[2], 10);
-      const milliseconds = Number.parseInt(match[3], 10);
-
-      return minutes * 60 + seconds + milliseconds / 1000;
-    }
-    if (match[4]) {
-      // Case with only seconds and milliseconds (e.g., "10.360")
-      const seconds = Number.parseInt(match[4], 10);
-      const milliseconds = Number.parseInt(match[5], 10);
-
-      return seconds + milliseconds / 1000;
-    }
-    throw new Error("Unexpected error in parsing");
-  }
 }
