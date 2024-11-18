8f662281c36021966d9617d309fa703915d7f81c
Thomas Dijkmans <tdijkmans@gmail.com>
2024-11-18 15:22:42 +0100

    feat: implement app menu component; add template, styles, and unit tests for improved navigation and user interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
 import { CommonModule } from "@angular/common";
 import { Component, DestroyRef, type OnInit, inject } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { FormsModule } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { ActivitieslistComponent } from "@components/activitieslist/activitieslist.component";
 import { ActivitystatsComponent } from "@components/activitystats/activitystats.component";
+import { AppMenuComponent } from "@components/app-menu/app-menu.component";
 import { BarchartComponent } from "@components/barchart/barchart.component";
 import { CircleBadgeComponent } from "@components/circle-badge/circle-badge.component";
 import { HistochartComponent } from "@components/histochart/histochart.component";
-import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import { provideIcons } from "@ng-icons/core";
 import {
   letsArrowLeft,
   letsArrowRight,
   letsSettingLine,
 } from "@ng-icons/lets-icons/regular";
 import { DataService } from "@services/dataservice/data.service";
-import { BehaviorSubject, type Observable, combineLatest, distinctUntilChanged, map, of } from "rxjs";
+import {
+  BehaviorSubject,
+  type Observable,
+  combineLatest,
+  distinctUntilChanged,
+  map,
+  of,
+} from "rxjs";
 import type { ChartTabVariant, SeasonTabVariant } from "./app.interface";
 import { DistchartComponent } from "./components/distchart/distchart.component";
 import { DistchartSeasonComponent } from "./distchart-season/distchart-season.component";
@@ -25,16 +32,15 @@ import type { Activity } from "./services/dataservice/data.interface";
   selector: "app-root",
   standalone: true,
   imports: [
-    FormsModule,
     CommonModule,
     ActivitystatsComponent,
     ActivitieslistComponent,
-    NgIconComponent,
     CircleBadgeComponent,
     BarchartComponent,
     HistochartComponent,
     DistchartComponent,
-    DistchartSeasonComponent
+    DistchartSeasonComponent,
+    AppMenuComponent,
   ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
@@ -47,67 +53,57 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   title = "";
   chipInput = "";
-  errorMessage = "";
-  menuOpen = false;
 
   chartTab = new BehaviorSubject<ChartTabVariant>("distance");
   seasonTab = new BehaviorSubject<SeasonTabVariant>("current");
 
   currentData$: Observable<{
-    currentActivity: Activity; curActivities: Activity[]; prevActivities: Activity[];
-  }>
+    currentActivity: Activity;
+    curActivities: Activity[];
+    prevActivities: Activity[];
+  }>;
 
   constructor(
     private d: DataService,
     private r: ActivatedRoute,
     private url: Router,
   ) {
-
-
     this.currentData$ = combineLatest([
       this.d.currentActivity$,
       this.d.curActivities$,
-      this.d.prevActivities$
+      this.d.prevActivities$,
     ]).pipe(
       map(([currentActivity, curActivities, prevActivities]) => ({
         currentActivity,
         curActivities,
-        prevActivities
-      }))
+        prevActivities,
+      })),
     );
-
   }
 
   ngOnInit() {
-    this.handleChipInput();
-
-  }
-
-  onClick(chipCode: string) {
-    if (!chipCode) {
-      this.errorMessage = "Vul een chipcode in";
-      return;
-    }
-    this.errorMessage = "";
-    this.d.setItem("chipCode", chipCode);
     this.handleChipInput();
   }
 
   handleChipInput() {
     const localChipCode$ = of(this.d.getItem<string>("chipCode"));
-    const routeChipCode$ = this.r.queryParams.pipe<string>(map((params) => params["transponder"]));
+    const routeChipCode$ = this.r.queryParams.pipe<string>(
+      map((params) => params["transponder"]),
+    );
 
     combineLatest([routeChipCode$, localChipCode$])
-      .pipe(
-        takeUntilDestroyed(this.destroyRef),
-        distinctUntilChanged()
-      )
+      .pipe(takeUntilDestroyed(this.destroyRef), distinctUntilChanged())
       .subscribe(([routeChipCode, localChipCode]) => {
         const chipCode = routeChipCode || localChipCode;
-        if (!chipCode) { return; }
+        if (!chipCode) {
+          return;
+        }
 
         // Initialize with the chipCode
-        this.d.init(chipCode).pipe(takeUntilDestroyed(this.destroyRef)).subscribe();
+        this.d
+          .init(chipCode)
+          .pipe(takeUntilDestroyed(this.destroyRef))
+          .subscribe();
 
         // Update local storage and chip input
         this.d.setItem("chipCode", chipCode);
@@ -115,7 +111,6 @@ export class AppComponent implements OnInit {
 
         // Update query params if necessary
         if (!routeChipCode) {
-          console.log("Navigating to", chipCode);
           this.url.navigate([], {
             relativeTo: this.r,
             queryParams: { transponder: chipCode },
@@ -124,7 +119,6 @@ export class AppComponent implements OnInit {
         }
       });
   }
-
 
   handleAct(activity: Activity) {
     this.d.setCurrentActivity(activity);
@@ -142,16 +136,6 @@ export class AppComponent implements OnInit {
       .navigateActivity("previous")
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe();
-  }
-
-  clearCache() {
-    this.d.clearLocalStorage();
-  }
-
-
-
-  toggleRightMenu() {
-    this.menuOpen = !this.menuOpen;
   }
 
   setTab(tab: ChartTabVariant) {
