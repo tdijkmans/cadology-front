f7e1e35342f39500647756674116c3abd3a34567
Thomas Dijkmans <tdijkmans@gmail.com>
2024-12-02 13:48:28 +0100

    feat: implement NotesComponent with input functionality and integrate into HomeComponent

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { letsAddRing, letsNotebook } from '@ng-icons/lets-icons/regular';
+import { Activity } from '@services/dataservice/data.interface';
 
 @Component({
   selector: 'cad-notes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, NgIconComponent],
   templateUrl: './notes.component.html',
-  styleUrl: './notes.component.scss'
+  styleUrl: './notes.component.scss',
+  viewProviders: [provideIcons({ letsAddRing, letsNotebook })],
 })
-export class NotesComponent {
+export class NotesComponent implements OnChanges {
+  @Input({ required: true }) currentActivity: Activity | null = null;
+
+  notes: string[] = [];
+  note = '';
+
+  constructor() { }
+
+  ngOnChanges() {
+    this.initializeData();
+  }
+
+  initializeData() {
+    console.log('initializeData', { currentActivity: this.currentActivity });
+  }
+
+  addNote() {
+    console.log('addNote');
+    this.notes.push(this.note);
+  }
 
 }
