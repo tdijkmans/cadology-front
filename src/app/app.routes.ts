2a6928c05bfd19f03a512c14beb25ca09ac4f9c9
Thomas Dijkmans <tdijkmans@gmail.com>
2025-01-13 21:08:37 +0100

    feat: remove unused Canvas and Video components; clean up routing and dependencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,7 @@
 import type { Routes } from '@angular/router';
-import { CanvasComponent } from '@pages/canvas/canvas.component';
 import { HomeComponent } from '@pages/home/home.component';
-import { VideoComponent } from '@pages/video/video.component';
 
 export const routes: Routes = [
-  {
-    path: 'canvas',
-    component: CanvasComponent,
-  },
-  {
-    path: 'video',
-    component: VideoComponent,
-  },
   {
     path: '',
     component: HomeComponent,
